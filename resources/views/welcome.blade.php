fcc6d3eae0a0121a54eb792832aea3562096f638
Richard Hsu <rhsu@gwu.edu>
2015-10-16 23:41:35 -0400

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/lumen/bootstrap.min.css">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <style>
+            
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <h1>Developer's Best Friend</h1>
+                <button type="button" class="btn btn-primary">Primary</button>
+            </div>
+        </div>
+    </body>
+</html>
